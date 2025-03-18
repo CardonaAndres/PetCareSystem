4cef05b0eb9bbe9a3000ac3df463fc1c972796b9
Andrés Cardona <135064128+Totoy777@users.noreply.github.com>
2025-03-18 14:52:07 -0500

    Configuración del CORS

--- src/configs/app.js
+++ src/configs/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import path from 'path';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import AuthRouter from '../modules/auth/router.js';
 import PetsRouter from '../modules/pets/router.js';
@@ -23,6 +24,12 @@ app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    credentials: true,  
+    methods: ['GET', 'POST', 'PUT', 'DELETE'] 
+}));
 
 app.use('/API/auth', AuthRouter);
 app.use('/API/pets', authMiddleware, PetsRouter);
